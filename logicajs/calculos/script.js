5bab5bea587ee9081d82017ce9441aec79e26369
calielmoras <158202187+calielmoras@users.noreply.github.com>
2026-04-30 09:30:24 -0300

    Add files via upload

---
+++ logicajs/calculos/script.js
@@ -0,0 +1,14 @@
+function somar (){
+    var numero1 = document.getElementById("n1").valueAsNumber
+    var numero2 = document.getElementById("n2").valueAsNumber
+    var res = document.getElementById("res")
+    res.textContent= numero1 + numero2
+}
+
+    function dividir(){
+        var n3 = parseFloat(document.getElementById)
+        var n4 = parseFloat(document.getElementById("n4").value)
+        var resp = document.getElementById("respDiv")
+        
+    }
+        console.log(n3)
